6f7728fa94e0088dbee6b304fd7233de49db5393
Ivan-Yagilev <ivanrosk@mail.ru>
2024-04-07 14:53:50 +0300

    add front

---
+++ static/server.js
@@ -0,0 +1,26 @@
+// Подключение необходимых модулей
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Создание сервера
+const server = http.createServer((req, res) => {
+  // Получение пути к запрашиваемому файлу
+  const filePath = path.join(__dirname, 'index.html');
+  
+  // Чтение HTML файла
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+// Запуск сервера на порту 3000
+server.listen(3000, () => {
+  console.log('Сервер запущен на http://localhost:3000/');
+});
